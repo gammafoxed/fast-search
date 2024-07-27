98344de7acb6acc67e6910f79b95edeeaf047cca
Aleksei Simakov <57300619+gammafoxed@users.noreply.github.com>
2024-07-27 17:03:59 +0300

    Some changes

--- ResourceTracker.fs
+++ ResourceTracker.fs
@@ -3,18 +3,24 @@ module fast_search.ResourceTracker
 open System.Diagnostics
 open System.Windows.Forms
 
-module private tracker =
+module private data =
     let performanceCounterCpu = new PerformanceCounter("Processor", "% Processor Time", "_Total")
     let performanceCounterMem = new PerformanceCounter("Memory", "Available MBytes")
+    
+    let mutable cpuThreshold = 80f;
+    let mutable memThreshold = 1024f;
+
+open data
+let startTracking(cpuUsageCallback: string -> unit)(memoryUsageCallback: string -> unit) =
     let timer = new Timer(Interval = 1000)
     timer.Tick.Add(fun _ ->
         let cpuUsage = performanceCounterCpu.NextValue()
         let memoryUsage = performanceCounterMem.NextValue()
-        let cpuThreshold = float(cpuThresholdTextBox.Text)
-        let memoryThreshold = float(memoryThresholdTextBox.Text)
+        let cpuThreshold = cpuThreshold
+        let memoryThreshold = memThreshold
 
-        cpuUsageLabel.Text <- sprintf "CPU Usage: %.2f%%" cpuUsage
-        memoryUsageLabel.Text <- sprintf "Memory Usage: %.2f MB" (float(SystemInfo.TotalPhysicalMemory - SystemInfo.AvailablePhysicalMemory) / (1024.0 * 1024.0))
+        sprintf "CPU Usage: %.2f%%" cpuUsage |> cpuUsageCallback
+        $"Memory Usage: %.2f{float(SystemInfo.TotalPhysicalMemory - SystemInfo.AvailablePhysicalMemory) / (1024.0 * 1024.0)} MB" |> memoryUsageCallback
 
         if cpuUsage > cpuThreshold || memoryUsage < memoryThreshold then
             cts.Cancel()
