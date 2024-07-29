6baaede14b4a30fce5501f45ec4f327b0c8980c3
Aleksei Simakov <57300619+gammafoxed@users.noreply.github.com>
2024-07-29 22:42:03 +0300

    Функиця для распараллеливания поиска работает. Но съедает огромное количество памяти и очень сильно грузит процессор. Возможно стоит сделать ограничение по количеству одновременно запускаемых асинхронных задач, а так же сделать асинхронный вывод данных (например через последовательности)

--- Program.fs
+++ Program.fs
@@ -1,63 +1,78 @@
 ﻿module fast_search.Program
 
+open System
+open System.Collections.Concurrent
+open System.IO
 open System.Threading
-open System.Threading.Tasks
-open System.Windows.Forms
-open ResourceTracker
-open UserInterface
-open Buttons
-open TextBox
-open ListBox
-open Domain
-open Labels
+
+
+module AsyncExtensions =
+    open Microsoft.FSharp.Control
+    let ParallelThrottled<'T> (maxDegreeOfParallelism: int) (jobs: seq<Async<'T>>) : Async<'T[]> =
+        async {
+            let semaphore = new SemaphoreSlim(maxDegreeOfParallelism, maxDegreeOfParallelism)
+            let resultBag = ConcurrentBag<'T>()
+
+            let runJob job = async {
+                do! semaphore.WaitAsync() |> Async.AwaitTask
+                try
+                    let! result = job
+                    resultBag.Add(result)
+                finally
+                    semaphore.Release() |> ignore
+            }
+
+            let! tasks = 
+                jobs 
+                |> Seq.map runJob 
+                |> Async.Parallel
+            
+            return resultBag.ToArray()
+        }
+
+let searchFilesAsync (rootDir: string) (pattern: string) (maxDegreeOfParallelism: int) (cancellationToken: CancellationToken) =
+    let files = ConcurrentBag<string>()
+    
+    let rec searchDirAsync (dir: string) = async {
+        try
+            // Добавляем найденные файлы в список
+            let fileEntries = Directory.GetFiles(dir, pattern)
+            for file in fileEntries do
+                files.Add(file)
+
+            // Рекурсивно ищем в поддиректориях
+            let subDirs = Directory.GetDirectories(dir)
+            let subDirJobs = 
+                subDirs 
+                |> Array.map (fun subDir -> async {
+                    do! searchDirAsync subDir
+                })
+            let! _ = AsyncExtensions.ParallelThrottled maxDegreeOfParallelism subDirJobs
+            return ()
+        with
+        | :? UnauthorizedAccessException -> return ()
+        | :? DirectoryNotFoundException -> return ()
+    }
+
+    async {
+        try
+            do! searchDirAsync rootDir
+        with
+        | :? OperationCanceledException -> ()
+        return files |> Seq.toList
+    }
 
 
 [<EntryPoint>]
 let main argv =
-    let mutable searchTask = Task.FromResult(())
-    let mutable cts = new CancellationTokenSource()
+    let cts = new CancellationTokenSource()
+    let token = cts.Token
+    let rootDir = @"/var/home/aleksei"
+    let pattern = "*.fs"
+    let maxDegreeOfParallelism = 4
 
-    // Инициализация формы
-    initForm ()
-
-    // Настройка кнопок
-    onClickDirButton (fun _ ->
-        use folderDialog = new FolderBrowserDialog()
-
-        if folderDialog.ShowDialog() = DialogResult.OK then
-            setDirectoryText folderDialog.SelectedPath)
-
-    onClickCancelButton (fun _ -> cts.Cancel())
-
-    onClickSearchButton (fun _ ->
-        let directory = getDirectoryText ()
-        let pattern = getPatternText ()
-        clearList ()
-        cts.Cancel()
-        cts.Dispose()
-        cts <- new CancellationTokenSource()
-
-        let asyncSearch = parallelFind directory pattern 4 cts.Token updateList
-
-        Async.StartWithContinuations(
-            asyncSearch,
-            (fun () -> MessageBox.Show("Search completed.") |> ignore),
-            (fun ex -> MessageBox.Show("Search encountered an error.") |> ignore),
-            (fun _ -> MessageBox.Show("Search was canceled.") |> ignore)
-        ))
-
-    // Запуск мониторинга ресурсов
-    let monitorTask =
-        startTrackingAsync
-            (fun cpuUsage -> updateCpuUsage cpuUsage)
-            (fun memoryUsage -> updateMemoryUsage memoryUsage)
-            (fun _ -> cts.Cancel())
-            cts.Token
-        |> Async.StartAsTask
-
-    runApplication ()
-
-    cts.Cancel()
-    monitorTask.Wait()
-
+    let task = searchFilesAsync rootDir pattern maxDegreeOfParallelism token
+    let result = Async.RunSynchronously task
+    
+    Console.WriteLine result
     0
